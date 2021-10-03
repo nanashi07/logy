ed20976704e8194c41b7523995e76943ad3a199b
Bruce Tsai <bruce.tsai@opennet.tw>
2021-10-03 08:58:19 +0000

    update match to if pattern

--- src/tracer/mod.rs
+++ src/tracer/mod.rs
@@ -29,37 +29,30 @@ pub fn trace_log(
         info!("Load file {} to collect cost time", file);
         let mut reader = WrappedFileReader::new(file, pattern, true);
         while let Log::Line(line) = reader.next_log() {
-            match re.captures(line.as_str()) {
-                Some(captures) => {
-                    let trace_id = captures.get(1).unwrap().as_str().to_string();
-                    // TODO: slice time issue, need by variable
-                    let log_time =
-                        NaiveDateTime::parse_from_str(&line.clone()[0..23], log_time_format)
-                            .unwrap();
-                    let log_time_milli = log_time.timestamp_millis();
+            if let Some(captures) = re.captures(line.as_str()) {
+                let trace_id = captures.get(1).unwrap().as_str().to_string();
+                // TODO: slice time issue, need by variable
+                let log_time =
+                    NaiveDateTime::parse_from_str(&line.clone()[0..23], log_time_format).unwrap();
+                let log_time_milli = log_time.timestamp_millis();
 
-                    match map.get(&trace_id) {
-                        Some(item) => {
-                            let newone = LogDuration {
-                                trace_id: item.trace_id.to_string(),
-                                start_time: cmp::min(item.start_time, log_time_milli),
-                                end_time: cmp::max(item.end_time, log_time_milli),
-                            };
-                            &map.insert(trace_id, newone);
-                        }
-                        None => {
-                            &map.insert(
-                                trace_id.clone(),
-                                LogDuration {
-                                    trace_id: trace_id.clone(),
-                                    start_time: log_time_milli,
-                                    end_time: log_time_milli,
-                                },
-                            );
-                        }
-                    }
+                if let Some(item) = map.get(&trace_id) {
+                    let newone = LogDuration {
+                        trace_id: item.trace_id.to_string(),
+                        start_time: cmp::min(item.start_time, log_time_milli),
+                        end_time: cmp::max(item.end_time, log_time_milli),
+                    };
+                    &map.insert(trace_id, newone);
+                } else {
+                    &map.insert(
+                        trace_id.clone(),
+                        LogDuration {
+                            trace_id: trace_id.clone(),
+                            start_time: log_time_milli,
+                            end_time: log_time_milli,
+                        },
+                    );
                 }
-                None => {}
             }
         }
 
@@ -90,48 +83,37 @@ pub fn trace_log(
 
         info!("start to output time cost logs from {}", file);
         while let Log::Line(line) = reader.next_log() {
-            match re.captures(line.as_str()) {
-                Some(captures) => {
-                    let trace_id = captures.get(1).unwrap().as_str().to_string();
-                    // TODO: slice time issue, need by variable
-                    let log_time =
-                        NaiveDateTime::parse_from_str(&line.clone()[0..23], log_time_format)
-                            .unwrap();
-                    let log_time_milli = log_time.timestamp_millis();
-                    let log_hour = log_time_milli / Duration::hours(1).num_milliseconds();
+            if let Some(captures) = re.captures(line.as_str()) {
+                let trace_id = captures.get(1).unwrap().as_str().to_string();
+                // TODO: slice time issue, need by variable
+                let log_time =
+                    NaiveDateTime::parse_from_str(&line.clone()[0..23], log_time_format).unwrap();
+                let log_time_milli = log_time.timestamp_millis();
+                let log_hour = log_time_milli / Duration::hours(1).num_milliseconds();
 
-                    match filtered.get(&trace_id) {
-                        Some(duration) => {
-                            match grouped_logs.get_mut(&trace_id) {
-                                Some(value) => {
-                                    value.push(line);
-                                }
-                                None => {
-                                    grouped_logs.insert(trace_id.clone(), vec![line]);
-                                }
-                            }
+                if let Some(duration) = filtered.get(&trace_id) {
+                    if let Some(value) = grouped_logs.get_mut(&trace_id) {
+                        value.push(line);
+                    } else {
+                        grouped_logs.insert(trace_id.clone(), vec![line]);
+                    }
 
-                            if log_time_milli >= duration.end_time {
-                                let v = grouped_logs.get_mut(&trace_id).unwrap();
-                                v.insert(
-                                    0,
-                                    format!(
-                                        "========================= {} =========================",
-                                        Duration::milliseconds(
-                                            duration.end_time - duration.start_time
-                                        )
-                                    ),
-                                );
-                                v.push("\n".repeat(3));
-                                // TODO: write log
-                                writer.write(log_hour, &v.join("\n"));
-                                grouped_logs.remove(&trace_id);
-                            }
-                        }
-                        None => {}
+                    // TODO: compare time
+                    if log_time_milli >= duration.end_time {
+                        let v = grouped_logs.get_mut(&trace_id).unwrap();
+                        v.insert(
+                            0,
+                            format!(
+                                "========================= {} =========================",
+                                Duration::milliseconds(duration.end_time - duration.start_time)
+                            ),
+                        );
+                        v.push("\n".repeat(3));
+                        // write log
+                        writer.write(log_hour, &v.join("\n"));
+                        grouped_logs.remove(&trace_id);
                     }
                 }
-                None => {}
             }
         }
         info!("finish output time cost logs from {}", file);
